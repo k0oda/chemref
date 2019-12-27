1ff061fcc896e8f477c9355cc493b12cf7f5cfda
kukree <kulevgen32@gmail.com>
2019-12-28 00:04:41 +0600

    Initial commit, add simple line by line json parsing

---
+++ ChemRef/ChemRef.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include "json.hpp"
+
+using namespace std;
+using namespace nlohmann;
+
+int main()
+{
+    ifstream database;
+    database.open("periodic_table.json", ios::in);
+
+    if (database.is_open())
+    {
+        string line;
+        json element;
+
+        while (getline(database, line))
+        {
+            element = json::parse(line);
+            cout << element.dump(4) << endl;
+        }
+        database.close();
+    }
+    else
+    {
+        cout << "Unable to load database";
+    }
+}
