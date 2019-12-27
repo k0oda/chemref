11446810f8d911341b4c00216e2be898bb046914
kukree <kulevgen32@gmail.com>
2019-12-28 00:44:35 +0600

    Add get_element_by_symbol function, add simple IO to main

--- ChemRef/ChemRef.cpp
+++ ChemRef/ChemRef.cpp
@@ -5,7 +5,22 @@
 using namespace std;
 using namespace nlohmann;
 
+json get_element_by_symbol(string symbol);
+
 int main()
+{
+    string input;
+    while (true)
+    {
+        cout << "> ";
+        cin >> input;
+        system("cls");
+        json element = get_element_by_symbol(input);
+        cout << element << endl;
+    }
+}
+
+json get_element_by_symbol(string symbol)
 {
     ifstream database;
     database.open("periodic_table.json", ios::in);
@@ -18,7 +33,11 @@ int main()
         while (getline(database, line))
         {
             element = json::parse(line);
-            cout << element.dump(4) << endl;
+            if (element["symbol"] == symbol)
+            {
+                database.close();
+                return element;
+            }
         }
         database.close();
     }
