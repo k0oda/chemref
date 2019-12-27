d63bfc497914e44568a621b7b877b79db5db87ab
kukree <kulevgen32@gmail.com>
2019-12-28 00:55:32 +0600

    Add get_input function

--- ChemRef/ChemRef.cpp
+++ ChemRef/ChemRef.cpp
@@ -6,18 +6,34 @@ using namespace std;
 using namespace nlohmann;
 
 json get_element_by_symbol(string symbol);
+string get_input();
 
 int main()
 {
     string input;
     while (true)
     {
-        cout << "> ";
-        cin >> input;
-        system("cls");
+        input = get_input();
         json element = get_element_by_symbol(input);
         cout << element << endl;
     }
+}
+
+string get_input()
+{
+    string input;
+    cout << "Enter element symbol to get full description of element or Q/q to exit" << endl;
+    cout << "> ";
+    cin >> input;
+    
+    if (input == "Q" || input == "q")
+    {
+        exit(0);
+    }
+
+    system("cls");
+
+    return input;
 }
 
 json get_element_by_symbol(string symbol)
