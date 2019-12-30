8a6db4106da2afb76e14735fa078b11fef30b2f6
kukree <kulevgen32@gmail.com>
2019-12-30 22:47:51 +0600

    Add output_element function

--- ChemRef/ChemRef.cpp
+++ ChemRef/ChemRef.cpp
@@ -7,6 +7,7 @@ using namespace nlohmann;
 
 json get_element_by_symbol(string);
 string get_input();
+void output_element(json);
 
 int main()
 {
@@ -15,8 +16,30 @@ int main()
     {
         input = get_input();
         json element = get_element_by_symbol(input);
-        cout << element << endl;
+        output_element(element);
     }
+}
+
+void output_element(json element)
+{
+    cout << "Name: " << element["name"] << endl;
+    cout << "Symbol: " << element["symbol"] << endl;
+    cout << "Appearance: " << element["appearance"] << endl;
+    cout << "Atomic mass: " << element["atomic_mass"] << endl;
+    cout << "Boil: " << element["boil"] << endl;
+    cout << "Category: " << element["category"] << endl;
+    cout << "Color: " << element["color"] << endl;
+    cout << "Density: " << element["density"] << endl;
+    cout << "Discovered by: " << element["discovered_by"] << endl;
+    cout << "Melting point: " << element["melt"] << endl;
+    cout << "Molar heat: " << element["molar_heat"] << endl;
+    cout << "Named by: " << element["named_by"] << endl;
+    cout << "Period: " << element["period"] << endl;
+    cout << "Phase: " << element["phase"] << endl;
+    cout << "Source: " << element["source"] << endl;
+    cout << "Spectral image: " << element["spectral_img"] << endl;
+    cout << "Summary: " << element["summary"] << endl;
+    cout << "Shells: " << element["shells"] << endl;
 }
 
 string get_input()
